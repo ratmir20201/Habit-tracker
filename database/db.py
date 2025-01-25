47ab6c0d8d0d7350657064e4891314dbb51cbf90
ratmir <ratmir2020.1@gmail.com>
2025-01-25 18:53:15 +0500

    добавил инициализацию базы данных и сессии

---
+++ database/db.py
@@ -0,0 +1,42 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import declarative_base
+
+from settings.config import settings
+
+DATABASE_URL = settings.db_url
+engine = create_async_engine(DATABASE_URL, echo=settings.echo)
+
+
+_Session = async_sessionmaker(
+    bind=engine,
+    autoflush=False,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+Base = declarative_base()
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    """Функция для получения сессии."""
+    async with _Session() as async_session:
+        yield async_session
+        await async_session.close()
+
+
+@asynccontextmanager
+async def get_async_context_session() -> AsyncGenerator[AsyncSession, None]:
+    """Функция для получения сессии используя ее в контекстном менеджере."""
+    async with _Session() as async_session:
+        try:
+            yield async_session
+        finally:
+            await async_session.close()
