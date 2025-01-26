b687113dc13217dd30cbae1598345d01e14fbf1f
ratmir <ratmir2020.1@gmail.com>
2025-01-26 09:39:29 +0500

    изменил config.py

--- settings/config.py
+++ settings/config.py
@@ -1,40 +1,48 @@
-import os
-
 from dotenv import load_dotenv
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 load_dotenv()
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_DB = os.getenv("POSTGRES_DB")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 
 class DbSettings(BaseSettings):
     """Настройки базы данных."""
 
-    db_url: str = "postgresql+asyncpg://{user}:{password}@postgres:5432/{db}".format(
-        user=POSTGRES_USER,
-        password=POSTGRES_PASSWORD,
-        db=POSTGRES_DB,
-    )
+    db_user: str = "user"
+    db_password: str = "password"
+    db_host: str = "postgres"
+    db_port: int = 5432
+    db_name: str = ""
     echo: bool = True
+
+    @property
+    def db_url(self) -> str:
+        return "postgresql+asyncpg://{user}:{password}@{host}:{port}/{name}".format(
+            user=self.db_user,
+            password=self.db_password,
+            host=self.db_host,
+            port=self.db_port,
+            name=self.db_name,
+        )
+
+    model_config = SettingsConfigDict(env_file=".env", env_prefix="POSTGRES_")
 
 
 class TelegramBotSettings(BaseSettings):
     """Настройки телеграмм бота."""
 
-    telegram_token: str = TELEGRAM_TOKEN
-    webhook_url: str = WEBHOOK_URL
+    token: str = ""
+    webhook_url: str = ""
+
+    model_config = SettingsConfigDict(env_file=".env", env_prefix="TELEGRAM_")
 
 
 class Settings(BaseSettings):
-    """Базовые настройки для приложения."""
+    """Общие настройки приложения."""
 
     tg_bot: TelegramBotSettings = TelegramBotSettings()
     db: DbSettings = DbSettings()
+
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
