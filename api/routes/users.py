0d3e0a73150d58737c8250b8aca5062a4484828b
ratmir <ratmir2020.1@gmail.com>
2025-02-01 16:46:34 +0500

    Добавил роутеры для взаимодействия с моделью пользователя

--- api/routes/users.py
+++ api/routes/users.py
@@ -1,3 +1,14 @@
 from fastapi import APIRouter
 
-router = APIRouter(tags=["Users"])
+from api.dependencies.fastapi_users_router import fastapi_users
+from api.schemas.user import UserRead, UserUpdate
+
+router = APIRouter(
+    tags=["Users"],
+    prefix="/api/users",
+)
+
+
+router.include_router(
+    router=fastapi_users.get_users_router(UserRead, UserUpdate),
+)
