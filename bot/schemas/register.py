99200029b6b0834d6503df7dd6fcc5edee18c118
ratmir <ratmir2020.1@gmail.com>
2025-02-06 17:13:20 +0500

    Создал схему регистрации для дальнейшей валидации

---
+++ bot/schemas/register.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, EmailStr, Field, root_validator, model_validator
+import re
+
+from typing_extensions import Self
+
+
+class RegisterSchema(BaseModel):
+    telegram_id: int
+    username: str
+    email: EmailStr
+    password: str = Field(..., min_length=8, pattern=r"^(?=.*[A-Z])(?=.*\d).*$")
+
+    @model_validator(mode="after")
+    def check_passwords_match(self) -> Self:
+        if len(self.password) < 8:
+            raise ValueError("Passwords do not match")
+        if not re.search(r"[A-Z]", self.password):
+            raise ValueError("Пароль должен содержать хотя бы одну заглавную букву.")
+        if not re.search(r"\d", self.password):
+            raise ValueError("Пароль должен содержать хотя бы одну цифру.")
+
+        return self
