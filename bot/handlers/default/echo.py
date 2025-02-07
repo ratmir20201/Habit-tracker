f20c5fed075e7ecaaa60fd956d4847ba35732fda
ratmir <ratmir2020.1@gmail.com>
2025-02-07 18:05:22 +0500

    добавил аннотацию Message

--- bot/handlers/default/echo.py
+++ bot/handlers/default/echo.py
@@ -2,7 +2,6 @@ from bot.main import tg_bot
 from telebot.types import Message
 
 
-
 @tg_bot.message_handler(func=lambda message: True)
 def echo_message(message: Message):
     tg_bot.reply_to(message, "К сожалению я не знаю такой команды.")
